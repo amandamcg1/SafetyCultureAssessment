ac7ac9a9b9302964181394083111949b3df1359b
Amanda McGilchrist <z5358641@unsw.edu.au>
2024-08-16 13:02:44 +1000

    tests written

--- sc-take-home-assessment-grad/folders/folders_test.go
+++ sc-take-home-assessment-grad/folders/folders_test.go
@@ -3,6 +3,8 @@ package folders_test
 import (
 	"testing"
 
+	// "github.com/georgechieng-sc/interns-2022/folders"
+	// "github.com/georgechieng-sc/interns-2022/folders"
 	"github.com/georgechieng-sc/interns-2022/folders"
 	"github.com/gofrs/uuid"
 	"github.com/stretchr/testify/assert"
@@ -12,11 +14,17 @@ import (
 func Test_GetAllFolders(t *testing.T) {
 	OrgID1 := folders.GetSampleData()[20].OrgId
 	OrgID2, err := uuid.NewV4()
+
+	if err != nil {
+		t.Fatalf("failed to generate new IIOD: %v", err)
+	}
+
 	t.Run("Successful retrieval of folders for a valid OrgID", func(t *testing.T) {
 		req := &folders.FetchFolderRequest{OrgID: OrgID1}
 		resp, err := folders.GetAllFolders(req)
 
 		assert.NoError(t, err, "Expected no error when fetching folders")
+		assert.NotNil(t, resp, "Expected a non-nil response")
 
 		expectedCount := 1
 		assert.Equal(t, expectedCount, len(resp.Folders), "Expected the correct number of folders")
@@ -36,4 +44,31 @@ func Test_GetAllFolders(t *testing.T) {
 		assert.NoError(t, resperr, "Expected no error when no folders are found")
 		assert.Equal(t, 0, len(resp.Folders), "Expected zero folders for a non-existing OrgID")
 	})
+
+	t.Run("Handle nil request input", func(t *testing.T) {
+		req := &folders.FetchFolderRequest{OrgID: OrgID2}
+		resp, err := folders.GetAllFolders(req)
+
+		assert.NoError(t, err, "Expected no error when no folders are found")
+		assert.NotNil(t, resp, "expected a non-nil response")
+		assert.Equal(t, 0, len(resp.Folders))
+	})
+
+	t.Run("Handle nil request input", func(t *testing.T) {
+		resp, err := folders.GetAllFolders(nil)
+
+		assert.Error(t, err, "Expected an error when request is nil")
+		assert.Nil(t, resp, "Expected a nil response when request is nil")
+	})
+
+	t.Run("Empty OrgID", func(t *testing.T) {
+		emptyOrgID := uuid.UUID{} // Represents an empty OrgID
+		req := &folders.FetchFolderRequest{OrgID: emptyOrgID}
+
+		resp, err := folders.GetAllFolders(req)
+
+		assert.NoError(t, err, "Expected no error when OrgID is empty")
+		assert.Equal(t, 0, len(resp.Folders), "Expected zero folders for an empty OrgID")
+	})
+
 }
